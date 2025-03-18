63ced23fd04b2d526a71258021e1299078c4554d
kim-elijah-sol <kimelijahsol@gmail.com>
2025-03-18 14:44:08 +0000

    Jupyter Notebook 적용, 편도 함수 그리기

--- index.py
+++ index.py
@@ -1,13 +1,1 @@
-from sympy import Derivative, symbols
-
-x = symbols('x')
-fx = 4*(x**4)+3*x
-
-f_prime = Derivative(fx, x).doit()
-print(f"fx의 도함수 {f_prime}")
-
-value_in_3 = f_prime.subs({ x: 3 })
-print(f"x=3에서의 기울기 : {value_in_3}")
-
-f_prime_prime = Derivative(f_prime, x).doit()
-print(f"fx의 2계도함수 {f_prime_prime}")
+print("mathematics-py")
